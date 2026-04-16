10cc24b7281f999d15ba24ff47416ce71018a95f
noble_kiwi <iwkr1208@gmail.com>
2026-04-17 00:30:43 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,38 +4,38 @@
 
 template<typename T, int size>
 class TStack {
-private:
-	static const int kSize = size;
-	T data[size];
-	int topIndex;
+ private:
+	 static const int kSize = size;
+	 T data[size];
+	 int topIndex;
 
-public:
-	TStack() : topIndex(-1) {}
-	void push(const T& value) {
-		if (topIndex < kSize - 1) {
-			data[++topIndex] = value;
-		}
-	}
-	T pop() {
-		if (!isEmpty()) {
-			return data[topIndex--];
-		}
-		return T();
-	}
+ public:
+	 TStack() : topIndex(-1) {}
+	 void push(const T& value) {
+		 if (topIndex < kSize - 1) {
+			 data[++topIndex] = value;
+		  }
+	  }
+	  T pop() {
+		  if (!isEmpty()) {
+			  return data[topIndex--];
+		  }
+		  return T();
+	  }
 
-	T top() const {
-		if (!isEmpty()) {
-			return data[topIndex];
-		}
-		return T();
-	}
+	  T top() const {
+		 if (!isEmpty()) {
+			 return data[topIndex];
+		 }
+		 return T();
+	 }
 
-	bool isEmpty() const {
-		return topIndex == -1;
-	}
-	bool isFull() const {
-		return topIndex == size - 1;
-	}
+	 bool isEmpty() const {
+		 return topIndex == -1;
+	 }
+	 bool isFull() const {
+		 return topIndex == size - 1;
+	 }
 };
 
 #endif // INCLUDE_TSTACK_H_
